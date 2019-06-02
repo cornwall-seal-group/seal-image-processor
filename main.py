94d0cd6da4872dd5503f3164cc7df5aa05f1c8f7
Richard Frost <richard.frost@headforwards.com>
2019-06-02 11:34:38 +0100

    Only look through originals folder

--- main.py
+++ main.py
@@ -27,11 +27,13 @@ def list_files(dir):
     r = []
     subdirs = [x[0] for x in os.walk(dir)]
     for subdir in subdirs:
-        files = os.walk(subdir).next()[2]
-        if (len(files) > 0):
-            for file in files:
-                r.append(subdir + "/" + file)
-    return r
+        # Only look through the orignals folders
+        if subdir == 'originals':
+            files = os.walk(subdir).next()[2]
+            if (len(files) > 0):
+                for file in files:
+                    r.append(subdir + "/" + file)
+    print r
 
 
 if __name__ == '__main__':
